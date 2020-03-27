7738bacfd15b32e18651db79d97125fb03e4c7fc
Eira Teufel <sv.tolstyakova@gmail.com>
2020-03-27 17:33:40 +0300

    изменен комментарий об ошибке чтения файла

--- routes/users.js
+++ routes/users.js
@@ -1,40 +1,36 @@
 const fs = require('fs');
 const usersRouter = require('express').Router();
-const noSuchUser =  {"message": "Нет пользователя с таким id"};
-const wrongLink = { "message": "Запрашиваемый ресурс не найден" };
 
 usersRouter.get('/users/:id', (req, res) => {
   fs.readFile('./data/user.json', 'utf8', (err, users) => {
     if (err) {
-        console.log("File read failed:", err)
-        return;
+      res.status(500).json({ message: err.message });
+      return;
     }
-    const {id} = req.params;
+    const { id } = req.params;
     const parsed = JSON.parse(users);
-    const user = parsed.find(item=>item._id===id);
+    const user = parsed.find(item => item._id === id);
     if (!user) {
 
-      res.status(404).send(noSuchUser);
-      return
+      res.status(404).json({"message": "Нет пользователя с таким id"});
+      return;
     }
     res.send(user);
   });
-  return;
 });
 
 usersRouter.get('/users', (req, res) => {
   fs.readFile('./data/user.json', 'utf8', (err, users) => {
     if (err) {
-        console.log("File read failed:", err)
-        return;
+      res.status(500).json({ message: err.message });
+      return;
     }
     res.send(users);
   });
-  return;
 });
 
 usersRouter.get('/:nonexistent', (req, res) => {
-  res.status(404).send(wrongLink);
+  res.status(404).json({"message": "Запрашиваемый ресурс не найден"});
 });
 
 module.exports = usersRouter;
