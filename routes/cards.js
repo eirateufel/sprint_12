9b4cf0d4906e659356ae1b84d2abb67b6bb84a2c
Eira Teufel <sv.tolstyakova@gmail.com>
2020-03-27 17:36:45 +0300

    оформление по eslint

--- routes/cards.js
+++ routes/cards.js
@@ -4,7 +4,7 @@ const cardsRouter = require('express').Router();
 cardsRouter.get('/cards', (req, res) => {
   fs.readFile('./data/cards.json', 'utf8', (err, cards) => {
     if (err) {
-      res.status(500).json({ "message": err.message });
+      res.status(500).json({ message: err.message });
       return;
     }
     res.send(cards);
